4f3c0cfe2dd75b2610289c5f367df5592b895486
Ximun <ximun@aquilenet.fr>
2021-01-14 16:10:51 +0100

    sops-test base

---
+++ configuration/hosts/sops-test/hardware-configuration.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ ];
+
+}
