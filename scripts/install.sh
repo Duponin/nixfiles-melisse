d90afd573fc3706a8625422a712174dd15dd48b8
Antonin Dupont <duponin@locahlo.st>
2021-01-16 16:07:48 +0100

    Move instructions in parenthesises
    It prevents premature execution, script has to be fully downloaded
    to be run

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,36 +2,38 @@
 
 set -e
 
-REPO="https://git.locahlo.st/chatons/nixfiles"
+(
+  REPO="https://git.locahlo.st/chatons/nixfiles"
 
-INSTALL_DEVICE="${1}"
-HOSTNAME="${2}"
+  INSTALL_DEVICE="${1}"
+  HOSTNAME="${2}"
 
-BOOT_DEVICE="${INSTALL_DEVICE}1"
-ROOT_DEVICE="${INSTALL_DEVICE}2"
+  BOOT_DEVICE="${INSTALL_DEVICE}1"
+  ROOT_DEVICE="${INSTALL_DEVICE}2"
 
-if [ -z "$HOSTNAME" ] || [ -z "$INSTALL_DEVICE" ]; then
-  echo "Please give arguments. <INSTALL_DEVICE> then <HOSTNAME>"
-  exit 1
-fi
+  if [ -z "$HOSTNAME" ] || [ -z "$INSTALL_DEVICE" ]; then
+    echo "Please give arguments. <INSTALL_DEVICE> then <HOSTNAME>"
+    exit 1
+  fi
 
-hostname "${HOSTNAME}"
+  hostname "${HOSTNAME}"
 
-parted --script "${INSTALL_DEVICE}" -- mklabel gpt
-parted --script "${INSTALL_DEVICE}" -- mkpart ESP fat32 1MiB 512MiB
-parted --script "${INSTALL_DEVICE}" -- set 1 esp on
-parted --script "${INSTALL_DEVICE}" -- mkpart primary 512MiB 100%FREE
+  parted --script "${INSTALL_DEVICE}" -- mklabel gpt
+  parted --script "${INSTALL_DEVICE}" -- mkpart ESP fat32 1MiB 512MiB
+  parted --script "${INSTALL_DEVICE}" -- set 1 esp on
+  parted --script "${INSTALL_DEVICE}" -- mkpart primary 512MiB 100%FREE
 
-mkfs.fat -F 32 -n boot "${BOOT_DEVICE}"
-mkfs.ext4 "${ROOT_DEVICE}" -L nixos
+  mkfs.fat -F 32 -n boot "${BOOT_DEVICE}"
+  mkfs.ext4 "${ROOT_DEVICE}" -L nixos
 
-mount "${ROOT_DEVICE}" /mnt
-mkdir /mnt/boot
-mount "${BOOT_DEVICE}" /mnt/boot
+  mount "${ROOT_DEVICE}" /mnt
+  mkdir /mnt/boot
+  mount "${BOOT_DEVICE}" /mnt/boot
 
-nix-env -iA nixos.git
-git clone "${REPO}" /mnt/etc/nixfiles
+  nix-env -iA nixos.git
+  git clone "${REPO}" /mnt/etc/nixfiles
 
-nixos-install --no-root-passwd -I "nixos-config=/mnt/etc/nixfiles/configuration/hosts/${HOSTNAME}/configuration.nix"
+  nixos-install --no-root-passwd -I "nixos-config=/mnt/etc/nixfiles/configuration/hosts/${HOSTNAME}/configuration.nix"
 
-reboot
+  reboot
+)
