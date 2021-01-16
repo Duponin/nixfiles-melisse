e57d32f175c00cd2f7b360b3d182f3baa544270d
Antonin Dupont <duponin@locahlo.st>
2021-01-16 15:15:39 +0100

    Add coreilla config

---
+++ configuration/hosts/coreilla/configuration.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }: {
+  imports = [ # Include the results of the hardware scan.
+    ../../common
+    ./hardware-configuration.nix
+  ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.systemd-boot.enable = true;
+
+  environment.systemPackages = with pkgs; [ borgbackup ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Set networking
+  networking = {
+    firewall = { allowedTCPPorts = [ 22 ]; };
+    hostName = "coreilla";
+    useDHCP = false;
+    nameservers = [ "2a0c:e300::100" "2a0c:e300::101" ];
+    interfaces = {
+      ens3 = {
+        mtu = 1500;
+        ipv6 = {
+          addresses = [{
+            address = "2a0c:e304:c0fe::1:2";
+            prefixLength = 48;
+          }];
+          routes = [{
+            address = "::";
+            prefixLength = 0;
+            via = "2a0c:e304:c0fe::1:1";
+          }];
+        };
+      };
+    };
+  };
+
+  services = { openssh.enable = true; };
+
+  system.stateVersion = "20.09";
+}
