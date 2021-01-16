f539a5a623f6005b8db1a616c9feb761e0cd6190
Antonin Dupont <duponin@locahlo.st>
2021-01-16 23:27:29 +0100

    Users are now immutable

--- configuration/common/default.nix
+++ configuration/common/default.nix
@@ -1,20 +1,23 @@
 { config, pkgs, lib, ... }: {
 
   environment.systemPackages = with pkgs; [ git ];
-  users.users = {
-    antonin = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "libvirtd" ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJh6W2o61dlOIcBXeWRhXWSYD/W8FDVf3/p4FNfL2L6p duponin@rilakkuma"
-      ];
-    };
-    simon = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "libvirtd" ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOiFv7rm8ChxvFaggUHRWcgGriWxkfiIPxhUSgTeA6n ximun@aquilenet.fr"
-      ];
+  users = {
+    mutableUsers = false;
+    users = {
+      antonin = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" "libvirtd" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJh6W2o61dlOIcBXeWRhXWSYD/W8FDVf3/p4FNfL2L6p duponin@rilakkuma"
+        ];
+      };
+      simon = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" "libvirtd" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOiFv7rm8ChxvFaggUHRWcgGriWxkfiIPxhUSgTeA6n ximun@aquilenet.fr"
+        ];
+      };
     };
   };
   security = {
