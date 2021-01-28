bddca8045a21950d5f43cd8cbc320d1723caaa20
Antonin Dupont <duponin@locahlo.st>
2021-01-28 21:47:41 +0100

    Fix sha256 in nixpkgs

--- deploy.nix
+++ deploy.nix
@@ -2,7 +2,7 @@ let
   # Pin the deployment package-set to a specific version of nixpkgs
   pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
-    sha256 = "a26a85bd98253f560c55ec48be1c793c39f471a6fb9842f9af07a701e854f9bf";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   }) { };
 in {
   network = {
