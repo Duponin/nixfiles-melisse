e4a889013b4f2b279ff52807c0030252d7bbbfd4
Antonin Dupont <duponin@locahlo.st>
2021-01-28 20:21:17 +0100

    Disable age support for now, it breaks IPv6 only hosts

--- configuration/common/qemu-guest/default.nix
+++ configuration/common/qemu-guest/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
-    "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/master.tar.gz"}/modules/age.nix"
+    # "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/master.tar.gz"}/modules/age.nix"
   ];
 
   boot.initrd.availableKernelModules =
