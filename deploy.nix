d1ea8fbe77da5da9ad7dcfaba330eda91a3a6411
Ximun <ximun@aquilenet.fr>
2021-01-29 12:03:04 +0100

    Don't refer anymore to different nixpkgs to deploy

--- deploy.nix
+++ deploy.nix
@@ -1,12 +1,5 @@
-let
-  # Pin the deployment package-set to a specific version of nixpkgs
-  pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
-    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
-  }) { };
-in {
+{
   network = {
-    inherit pkgs;
     description = "Mélisse hosts";
     ordering = { tags = [ "prod" ]; };
   };
