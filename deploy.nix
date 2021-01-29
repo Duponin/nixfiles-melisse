4b71151b9ddfe4c14df83d476f89eae4e03c197f
Ximun <ximun@aquilenet.fr>
2021-01-29 12:25:20 +0100

    Add all VMs to morph deploy file

--- deploy.nix
+++ deploy.nix
@@ -4,8 +4,28 @@
     ordering = { tags = [ "prod" ]; };
   };
 
+  "aedu.melisse.org" = { config, pkgs, ... }: {
+    deployment.tags = [ "prod" ];
+    imports = [ ./configuration/hosts/aedu/configuration.nix ];
+  };
+  "anoat.melisse.org" = { config, pkgs, ... }: {
+    deployment.tags = [ "prod" ];
+    imports = [ ./configuration/hosts/anoat/configuration.nix ];
+  };
   "coreilla.melisse.org" = { config, pkgs, ... }: {
     deployment.tags = [ "prod" ];
     imports = [ ./configuration/hosts/coreilla/configuration.nix ];
+  };
+  "malastare.melisse.org" = { config, pkgs, ... }: {
+    deployment.tags = [ "prod" ];
+    imports = [ ./configuration/hosts/malastare/configuration.nix ];
+  };
+  "rishi.melisse.org" = { config, pkgs, ... }: {
+    deployment.tags = [ "prod" ];
+    imports = [ ./configuration/hosts/rishi/configuration.nix ];
+  };
+  "sullust.melisse.org" = { config, pkgs, ... }: {
+    deployment.tags = [ "prod" ];
+    imports = [ ./configuration/hosts/sullust/configuration.nix ];
   };
 }
