2aedff4a844d33f1a77910123ac31a3d5a623511
Antonin Dupont <duponin@locahlo.st>
2021-04-14 20:42:37 +0200

    Add portainer domain

--- configuration/hosts/coruscant/docker.nix
+++ configuration/hosts/coruscant/docker.nix
@@ -6,6 +6,16 @@
     extraOptions = ''
       --ip="127.0.0.1" \
     '';
+  };
 
+  services.nginx.virtualHosts."portainer.melisse.org" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://localhost:9000";
+      extraConfig = ''
+        proxy_ssl_server_name on;
+      '';
+    };
   };
 }
