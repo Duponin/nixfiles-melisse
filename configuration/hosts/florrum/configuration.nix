2386e9eb2193e14e5aabf3cde64e4e13b693987e
Simon Crespeau <simon.crespeau@aquilenet.fr>
2021-04-22 17:40:26 +0200

    Add florrum configuration (wireguard distant backup host)

---
+++ configuration/hosts/florrum/configuration.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+let hostname = "florrum";
+in {
+  imports = [ # imports
+    ../../../modules/monitoring/client.nix
+    ../../common
+    ./hardware-configuration.nix
+  ];
+
+  monitoring.client.enable = "true";
+  monitoring.client.host = hostname;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    hostname = hostname;
+    useDHCP = false;
+    interfaces.enp2s0.useDHCP = true;
+    wireguard.interfaces = {
+      wg0 = {
+        listenPort = 51820;
+        privateKeyFile = "/root/wg_private_key";
+        ips = [ "2a0c:e304:c0fe:20::12/64" ];
+
+        peers = [{
+          publicKey = "ldEQJx37V3lA20QUvDbdEouP7SqHunOXink+pN0pynQ=";
+          allowedIPs = [ "::/0" ];
+          endpoint = "coruscant.melisse.org:51820";
+          persistentKeepalive = 25;
+        }];
+      };
+    };
+    firewall.allowedUDPPorts = [ 51820 ];
+  };
+
+  system.stateVersion = "20.09";
+}
---
+++ configuration/hosts/florrum/hardware-configuration.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ata_piix" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/root";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  fileSystems."/backups" =
+    { device = "/dev/disk/by-label/backups";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+}
