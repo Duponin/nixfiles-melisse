aafd7e16488f160334395bd001d324f2b45f37b7
Simon Crespeau <simon.crespeau@aquilenet.fr>
2021-04-22 14:56:31 +0200

    Add florrum wireguard peer

--- configuration/hosts/coruscant/wireguard.nix
+++ configuration/hosts/coruscant/wireguard.nix
@@ -11,10 +11,16 @@
         ips = [ "2a0c:e304:c0fe:20::1/64" ];
         listenPort = 51820;
         privateKeyFile = "/run/secrets/wireguard_privatekey";
-        peers = [{
-          publicKey = "LipBKld4iV7jYcE4tdIuscULUZs45+/g3BoPXZ4u41M=";
-          allowedIPs = [ "2a0c:e304:c0fe:20::11/128" ];
-        }];
+        peers = [
+          {
+            publicKey = "LipBKld4iV7jYcE4tdIuscULUZs45+/g3BoPXZ4u41M=";
+            allowedIPs = [ "2a0c:e304:c0fe:20::11/128" ];
+          }
+          {
+            publicKey = "p1+fAZ1if362mNI+5Lgi3v+NVUm2yEubEFMAqzeuPVk=";
+            allowedIPs = [ "2a0c:e304:c0fe:20::12/128" ];
+          }
+        ];
       };
     };
   };
