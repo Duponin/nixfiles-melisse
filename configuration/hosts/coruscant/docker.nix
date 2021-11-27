5d815ab4f1c9fb7725fdeab71adbea076ded8967
Antonin Dupont <duponin@locahlo.st>
2021-11-27 16:06:35 +0100

    No longer use Nginx in Docker configuration on Coruscant

--- configuration/hosts/coruscant/docker.nix
+++ configuration/hosts/coruscant/docker.nix
@@ -1,34 +1,10 @@
 { config, ... }:
 
 {
-  imports = [ ../../common/nginx.nix ];
-
   virtualisation.docker = {
     enable = true;
     extraOptions = ''
       --ip="127.0.0.1" \
     '';
-  };
-
-  services.nginx.virtualHosts."team.melisse.org" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://localhost:10003";
-      proxyWebsockets = true;
-      extraConfig = ''
-        proxy_ssl_server_name on;
-      '';
-    };
-  };
-  services.nginx.virtualHosts."portainer.melisse.org" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://localhost:9000";
-      extraConfig = ''
-        proxy_ssl_server_name on;
-      '';
-    };
   };
 }
