c41d1ad4ffcf2ac2403c449e6d384af87529549a
Antonin Dupont <contact+git@antonindupont.fr>
2022-03-26 16:04:28 +0100

    Update Morph deploy config

--- deploy.nix
+++ deploy.nix
@@ -1,33 +1,10 @@
 {
   network = {
     description = "Mélisse hosts";
-    ordering = { tags = [ "staging" "prod" ]; };
+    ordering = { tags = [ "prod" ]; };
   };
 
-  # STAGING HOSTS
-  "kessel.melisse.org" = { config, pkgs, ... }: {
-    deployment.tags = [ "staging" ];
-    imports = [ ./configuration/hosts/kessel/configuration.nix ];
-  };
-
-  # PROD HOSTS
-  "anoat.melisse.org" = { config, pkgs, ... }: {
-    deployment.tags = [ "prod" ];
-    imports = [ ./configuration/hosts/anoat/configuration.nix ];
-  };
-  "coreilla.melisse.org" = { config, pkgs, ... }: {
-    deployment.tags = [ "prod" ];
-    imports = [ ./configuration/hosts/coreilla/configuration.nix ];
-  };
-  "florrum.melisse.org" = { config, pkgs, ... }: {
-    deployment.tags = [ "prod" ];
-    imports = [ ./configuration/hosts/florrum/configuration.nix ];
-  };
-  "rishi.melisse.org" = { config, pkgs, ... }: {
-    deployment.tags = [ "prod" ];
-    imports = [ ./configuration/hosts/rishi/configuration.nix ];
-  };
-  "sullust.melisse.org" = { config, pkgs, ... }: {
+  "borg.melisse.org" = { config, pkgs, ... }: {
     deployment.tags = [ "prod" ];
     imports = [ ./configuration/hosts/sullust/configuration.nix ];
   };
